47b763c15a72c8716281e5c0644c4b9d51790212
Briana Wallace <brianahorton4@gmail.com>
2023-07-21 18:23:27 -0700

    Maybe a settings page too?

---
+++ chatter/src/Pages/UserSettingsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const UserSettingsPage=()=>{
+    return(
+        <div className="user-preferences">
+            usersettings
+        </div>
+    )
+}; export default UserSettingsPage
