ababd2f1a39b399be3278240ce5fbe5dc75b7513
Briana Wallace <brianahorton4@gmail.com>
2023-07-23 14:33:06 -0700

    ContactBox searchbar/contacts array list added (?)

--- chatter/src/Components/ContactBox.tsx
+++ chatter/src/Components/ContactBox.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 
 const ContactBox=()=>{
+const [value, setValue] = useState '';
+const onChange = (event) => {
+    setValue(event.target.value);
+}
+item = [];
     return(
-
-        <div className="w-[300px] h-[700px] bg-slate-600">
-        </div>
+        <>
+            <div className="w-[300px] h-[700px] bg-slate-600">
+                <h1>Search Contacts</h1>
+                <div className='search-container'>
+                    <div className='search-inner'>
+                        <input type="text" value={value} onChange={onChange}>
+                        </input>
+                    </div>
+                </div>
+                <h2>Contacts</h2>
+                {item.length === 0 && <p>No Contacts</p>}
+                    <ul className="list-group">
+                        {item.map((item) => (
+                            <li key={item}>
+                                {item}
+                            </li>
+                         ))}
+                    </ul>
+            </div>
+        </>
     )
 }; export default ContactBox
